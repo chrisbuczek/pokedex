acd9846213861a7c9132b60d0b57fd7224e37b9b
chrisbuczek <krzysztofbuczek@live.com>
2025-07-24 05:19:23 +0200

    install tailwind v4 and mui intergration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Input from "@mui/material/Input";
+import FormHelperText from "@mui/material/FormHelperText";
 
 function App() {
   return (
     <>
       <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
+        <Button className="bg-red-500 text-black" variant="text">
+          Hello Tailwind v4
+        </Button>
         <Button variant="contained">Contained</Button>
         <Button variant="outlined">Outlined</Button>
       </Stack>
--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { StyledEngineProvider } from "@mui/material/styles";
+import GlobalStyles from "@mui/material/GlobalStyles";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <App />
+    </StyledEngineProvider>
   </StrictMode>
 );
