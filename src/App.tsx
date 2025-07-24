f32fc5cfe1db4bb1ae086c01cf298c8102ee7c82
chrisbuczek <krzysztofbuczek@live.com>
2025-07-24 05:02:38 +0200

    install mui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <>
-      <div>hello</div>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
     </>
   );
 }
